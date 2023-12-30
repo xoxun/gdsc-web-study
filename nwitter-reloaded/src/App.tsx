3fa4eae9fa67c9ecc5dc22f793a66b58d02d649a
xoxun <00xxs2@naver.com>
2023-12-31 05:18:18 +0900

    Basic routing

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -1,8 +1,29 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Layout from "./components/layout"
+import Home from "./routes/home"
+import Profile from "./routes/profile"
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<Layout />,
+  children :[
+    {
+    path: "",
+    element: <Home />,
+  },
+  {path: "profile",
+  element: <Profile />,
+  },
+]
+}])
+
+
 
 function App() {
   
   return (
     <>
+    <RouterProvider router={router}/>
     </>
   )
 }
