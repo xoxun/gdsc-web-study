37d2ed1a79f0ae5db173fa1dcc0766730e750258
xoxun <00xxs2@naver.com>
2023-12-31 05:23:06 +0900

    Basic routing 2

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -2,20 +2,30 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Layout from "./components/layout"
 import Home from "./routes/home"
 import Profile from "./routes/profile"
+import Login from "./routes/login"
+import CreateAccount from "./routes/create-account"
 
-const router = createBrowserRouter([{
-  path:"/",
-  element:<Layout />,
-  children :[
-    {
-    path: "",
-    element: <Home />,
-  },
-  {path: "profile",
-  element: <Profile />,
-  },
-]
-}])
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children :[
+      {
+      path: "",
+      element: <Home />,
+      },
+      {path: "profile",
+      element: <Profile />,
+      },
+    ]
+  },{
+    path: "/login",
+    element:<Login />
+  },{
+    path: "/create-account",
+    element: <CreateAccount />
+  }
+])
 
 
 
---
+++ nwitter-reloaded/src/routes/create-account.tsx
@@ -0,0 +1,4 @@
+export default function CreateAccount(){
+
+    return <h2>Create Account</h2>
+}
