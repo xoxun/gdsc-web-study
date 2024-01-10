b71d3a1706b1186fb1789ae71d463cc3250ef8bb
xoxun <00xxs2@naver.com>
2024-01-11 02:38:29 +0900

    theme darkmode

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -1,4 +1,9 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
+
+
+const Title = styled.h1`
+  color: ${(props)=>props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,44 +11,13 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0%{
-    transform  : rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform  : rotate(180);
-    border-radius: 100px;
-  }
-  100%{
-    transform  : rotate(360deg);
-    border-radius: 0px;
-  }`;
-
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 200px;
-  height: 200px;
-  background-color: pink;
-  animation: ${rotationAnimation} 1s linear infinite;
-  span{
-    font-size: 36px;
-    &:hover{
-      font-size: 60px;
-    }
-  }
+  background-color: ${(props)=>props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <span>🥰</span>
-      </Box>
+      <Title>Hellp</Title>
     </Wrapper>
   );
 }
