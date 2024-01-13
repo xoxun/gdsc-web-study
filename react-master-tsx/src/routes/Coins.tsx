9f0d71b6b03a35c33353f8aa1d6d0380d3bfab85
xoxun <00xxs2@naver.com>
2024-01-13 17:13:25 +0900

    Crypto Tracker price/chart tab

--- react-master-tsx/src/routes/Coins.tsx
+++ react-master-tsx/src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -55,7 +57,7 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-interface CoinInterface{
+interface ICoin{
   id: string,
   name: string,
   symbol: string,
@@ -66,7 +68,8 @@ interface CoinInterface{
 }
 
 function Coins(){
-
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins)
+/* 
   const [coins, setCoins] = useState<CoinInterface[]>([]);
   const [loading, setLoading] = useState(true);
   useEffect(()=>{
@@ -77,15 +80,16 @@ function Coins(){
       setLoading(false);
       console.log(coins);
     })();
-  },[])
+  },[]) */
+
     return <Container>
         <Header>
             <Title>Coin</Title>
         </Header>
-        {loading ?
+        {isLoading ?
         <Loader>Loading...</Loader> 
         : <CoinList>
-            {coins.map((coin)=>(
+            {data?.slice(0,100).map((coin)=>(
               <Coin key={coin.id}>
                 <Link to={{
                   pathname:`/${coin.id}`,
