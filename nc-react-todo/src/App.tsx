1dd63dc80d8e879ac355b7c5209eda4719e9729c
xoxun <00xxs2@naver.com>
2024-02-09 05:13:51 +0900

    todo 기본 틀&파일분리 리팩토링

--- nc-react-todo/src/App.tsx
+++ nc-react-todo/src/App.tsx
@@ -1,5 +1,6 @@
 import {createGlobalStyle} from "styled-components";
-import ToDoList from "./ToDoList";
+import ToDoList from "./components/ToDoList";
+import { RecoilRoot } from "recoil";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -61,8 +62,10 @@ const GlobalStyle = createGlobalStyle`
 function App() {
    return (
     <>
+    <RecoilRoot>
       <GlobalStyle />
       <ToDoList />
+    </RecoilRoot>
     </>
   );
 }
---
+++ nc-react-todo/src/components/ToDoList.tsx
@@ -0,0 +1,24 @@
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+import { toDoState } from "../atoms";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id}{...toDo}/>
+        ))}
+      </ul>
+      
+      
+    </div>
+  );
+}
+
+export default ToDoList;
