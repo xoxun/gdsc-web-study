5da742be67f7596fb466512bec5a5c60a6232752
xoxun <00xxs2@naver.com>
2024-02-09 06:05:08 +0900

    category state change

--- nc-react-todo/src/components/ToDo.tsx
+++ nc-react-todo/src/components/ToDo.tsx
@@ -1,15 +1,31 @@
+import { useSetRecoilState } from "recoil";
+import {IToDo, toDoState} from "../atoms";
 
-import {IToDo} from "../atoms";
-
-export default function ToDo({text}:IToDo){
-  
+export default function ToDo({text, category,id}:IToDo){
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory:IToDo["category"]) =>{
+    //console.log(newCategory);
+    setToDos(oldToDos =>{
+      const targetIndex = oldToDos.findIndex(toDo=>toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = {text, id, category:newCategory as any};
+      return [
+        ...oldToDos.slice(0,targetIndex), 
+        newToDo ,
+        ...oldToDos.slice(targetIndex+1)
+      ];
+    });
+  };
   return <>
     <li>
       <span>{text}</span> 
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && 
+        <button onClick={()=>onClick("DOING")}>To Do</button>}
+      {category !== "TO_DO" && 
+        <button onClick={()=>onClick("TO_DO")}>Doing</button>}
+      {category !== "DONE" && 
+        <button onClick={()=>onClick("DONE")}>Done</button>}
       
-      </li>
+    </li>
   </>;
 }
